923ca46f365562af492f539eff60911dd57a4407
ely-marquez <elymarquez@protonmail.com>
2021-09-21 19:04:36 +0700

    new

--- ClientApp/src/components/SearchBar.tsx
+++ ClientApp/src/components/SearchBar.tsx
@@ -25,30 +25,55 @@ type searchBarItemProps = {
 
 const SearchBar = () => {
   return (
-    <div className="d-flex">
-      <SearchBarItem caption="All" />
-      <SearchBarItem icon="chair" />
-      <SearchBarItem icon="chair2" />
-      <SearchBarItem icon="couch" />
-      <SearchBarItem icon="sofa" />
-      <SearchBarItem icon="sofa2" />
-      <SearchBarItem icon="table" />
-      <SearchBarItem icon="cupboard" />
-      <SearchBarItem icon="lamp" />
-      <SearchBarItem icon="lamp2" />
+    <div className="searchbar-wrapper">
+      {/* <div className="searchbar d-flex"> */}
+      {/* <div className="searchbar d-flex flex-column">
+        <SearchBarItem caption="All" />
+        <SearchBarItem icon="chair" />
+        <SearchBarItem icon="chair2" />
+        <SearchBarItem icon="couch" />
+        <SearchBarItem icon="sofa" />
+        <SearchBarItem icon="sofa2" />
+        <SearchBarItem icon="table" />
+        <SearchBarItem icon="cupboard" />
+        <SearchBarItem icon="lamp" />
+        <SearchBarItem icon="lamp2" />
+      </div> */}
+      <ul className="searchbar">
+        <SearchBarItem caption="All" />
+        <SearchBarItem icon="chair" />
+        <SearchBarItem icon="chair2" />
+        <SearchBarItem icon="couch" />
+        <SearchBarItem icon="sofa" />
+        <SearchBarItem icon="sofa2" />
+        <SearchBarItem icon="table" />
+        <SearchBarItem icon="cupboard" />
+        <SearchBarItem icon="lamp" />
+        <SearchBarItem icon="lamp2" />
+      </ul>
     </div>
   );
 };
 
 const SearchBarItem = ({ caption, icon }: searchBarItemProps): JSX.Element => {
   return (
-    <div className="searchbaritem bg-secondary mr-3 d-flex justify-content-center align-items-center">
+    // <div className="searchbaritem bg-secondary mr-3 d-flex justify-content-center align-items-center">
+    // <div className="searchbaritem bg-secondary mb-3 d-flex justify-content-center align-items-center">
+    //   {icon ? (
+    //     <img id={icon} alt="icon" className="bg-primary" />
+    //   ) : (
+    //     // <span className="bg-primary">{caption}</span>
+    //     <span>{caption}</span>
+    //   )}
+    // </div>
+    <li className="searchbaritem bg-secondary mb-3 d-flex justify-content-center align-items-center">
       {icon ? (
         <img id={icon} alt="icon" className="bg-primary" />
       ) : (
-        <span className="bg-primary">{caption}</span>
+        // <span className="bg-primary">{caption}</span>
+        <span>{caption}</span>
       )}
-    </div>
+    </li>
   );
 };
 
